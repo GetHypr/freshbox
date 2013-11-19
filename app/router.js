6a2ca0dc3b4d2bb8f877afdcd51c51bf4b169824
Hypr <jake@hyprinc.com>
2013-11-19 15:32:58 +0800

    New branch commit (not completed)

--- app/router.js
+++ app/router.js
@@ -1,16 +1,12 @@
-var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
+var Router = Ember.Router.extend();
 
 Router.map(function() {
-    this.route("about", { path: "/about" });
-    this.route("faq", { path: "/faq" });
-    this.route("ordertrack", { path: "track"});
-    this.route("myAccount", { path: "account" });
-
-    this.route('component-test');
-    this.route('helper-test');
-    // this.resource('posts', function() {
-    //   this.route('new');
-    // });
+  this.route('our-boxes');
+  this.resource('store', function() {
+    this.resource('category', {path:'/:category_id'}, function() {
+      this.resource('product', {path:'/:product_id'});
+    });
+  });
 });
 
 export default Router;
